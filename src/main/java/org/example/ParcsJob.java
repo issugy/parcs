40b5409c978d83e8e2833786a82ebdd2ef7083f0
ihor.selezen <ihor.selezen@pitchbook.com>
2024-06-09 19:10:51 +0300

    fix2

--- src/main/java/org/example/ParcsJob.java
+++ src/main/java/org/example/ParcsJob.java
@@ -1,25 +1,25 @@
 package org.example;
 
-import parcs.AMInfo;
-import parcs.task;
+import parcs.*;
+import java.nio.file.*;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class ParcsJob {
     public static void main(String[] args) {
         try {
-            // Додайте цей блок для перевірки
             String serverFilePath = "out/server";
             String serverIP = Files.readString(Paths.get(serverFilePath)).trim();
             System.out.println("Server IP read from file: " + serverIP);
 
             // Створюємо завдання
+            System.out.println("Creating task...");
             task curtask = new task();
             curtask.addJarFile("ShellSort.jar");
             AMInfo info = new AMInfo(curtask, null);
+            System.out.println("Running ShellSort...");
             (new ShellSort()).run(info);
             curtask.end();
+            System.out.println("Task ended.");
         } catch (IOException e) {
             System.err.println("Error reading server file: " + e.getMessage());
             e.printStackTrace();
